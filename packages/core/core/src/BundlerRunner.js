50a5085fbb778b2838e94a3013779b7e19a2c10f
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-05-09 23:14:17 -0700

    Wrap changedassets in public api

--- packages/core/core/src/BundlerRunner.js
+++ packages/core/core/src/BundlerRunner.js
@@ -145,7 +145,7 @@ export default class BundlerRunner {
       });
 
       // build a graph of just the transformed asset
-      let graph = await builder.build();
+      let {assetGraph: graph} = await builder.build();
 
       let entry = graph.getEntryAssets()[0];
       let subGraph = graph.getSubGraph(nullthrows(graph.getNode(entry.id)));
